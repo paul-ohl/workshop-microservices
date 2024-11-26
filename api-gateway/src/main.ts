8de639469da5854fb4619d7c7fc9d9932d78247b
Mahylan <mahylan.veclin@wangarden.com>
2024-11-26 21:21:52 +0100

    commit for pull

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
+const logger = new Logger('Bootstrap');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
