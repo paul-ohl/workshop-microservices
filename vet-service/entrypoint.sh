47441201334be6ffd90c071b8be4027271a9880c
Johanna Perso <johannaperso@MacBook-Pro-de-Johanna.local>
2024-11-29 10:00:02 +0100

    fix/prisma migration and docker compose build + remove auth middleware

---
+++ vet-service/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Attendre que la base de données soit prête
+until pg_isready -h "${PG_HOST}" -p 5432 -U "${PG_USER}"; do
+    echo "En attente de la base de données..."
+    sleep 2
+done
+
+# Exécute les migrations Prisma
+npx prisma migrate deploy
+
+npx prisma db push
+
+# Lance l'application
+npm run dev
--- vet-service/src/vet.module.ts
+++ vet-service/src/vet.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { VetService } from './vet.service';
 import { VetController } from './vet.controller';
 import { PrismaService } from 'prisma/prisma.service';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [VetController],
   providers: [VetService, PrismaService],
 })
