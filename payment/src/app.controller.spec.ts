74e59a5f211fcf331009889054fbec087250afde
Paul Ohl <paul.lv.ohl@gmail.com>
2024-12-01 17:59:47 +0100

    feat: Wrote tests for payment

--- payment/src/app.controller.spec.ts
+++ payment/src/app.controller.spec.ts
@@ -5,11 +5,23 @@ import { AppService } from './app.service';
 describe('AppController', () => {
   let appController: AppController;
 
+  const returnJson = JSON.stringify({
+    sessionId: '1234',
+    sessionUrl: 'http://example.com',
+  });
+
+  const mockAppService = {
+    getHello: jest.fn(() => 'OK from Payment microservice'),
+    payCart: jest.fn(async () => {
+      return returnJson;
+    }),
+  };
+
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
-    }).compile();
+    }).overrideProvider(AppService).useValue(mockAppService).compile();
 
     appController = app.get<AppController>(AppController);
   });
@@ -17,6 +29,13 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return OK', () => {
       expect(appController.getHello()).toBe('OK from Payment microservice');
+      expect(mockAppService.getHello).toHaveBeenCalled();
+    });
+    it('should call payCart function', async () => {
+      const payCartDto = { id: 1, items: [], ownerId: 'abcd' };
+      const res = await appController.payCart(payCartDto);
+      expect(res).toBe(returnJson);
+      expect(mockAppService.payCart).toHaveBeenCalledWith(payCartDto);
     });
   });
 });
