251ec187a4a69f6cab81f0e14393dfcbf88d82b1
Paul Ohl <paul.lv.ohl@gmail.com>
2024-12-02 15:37:15 +0100

    feat: Every service has their own database

--- vet/entrypoint.sh
+++ vet/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Attendre que la base de données soit prête
 until pg_isready -h "${PG_HOST}" -p 5432 -U "${PG_USER}"; do
@@ -6,10 +6,23 @@ until pg_isready -h "${PG_HOST}" -p 5432 -U "${PG_USER}"; do
     sleep 2
 done
 
+# Create the database if it doesn't already exist
+echo "Checking if database <${PG_NAME}> exists..."
+DB_EXISTS=$(PGPASSWORD="${PG_PASSWORD}" psql -h "${PG_HOST}" -U "${PG_USER}" -tAc "SELECT 1 FROM pg_database WHERE datname='${PG_NAME}'")
+
+if [ "$DB_EXISTS" != "1" ]; then
+    echo "Database ${PG_NAME} does not exist. Creating..."
+    PGPASSWORD="${PG_PASSWORD}" psql -h "${PG_HOST}" -U "${PG_USER}" -c "CREATE PG_NAME};"
+else
+    echo "Database ${PG_NAME} already exists."
+fi
+
+export DATABASE_URL=postgres://${PG_USER}:${PG_PASSWORD}@${PG_HOST}/${PG_NAME}?schema=public
+
 # Exécute les migrations Prisma
 npx prisma migrate deploy
 
 npx prisma db push
 
 # Lance l'application
-npm run dev
+npm run start:prod
