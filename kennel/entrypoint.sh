01f2989e73fa2cbf5d5b3e39e1327d98f598982d
Paul Ohl <paul.lv.ohl@gmail.com>
2024-12-02 11:38:45 +0100

    feat: Added prisma stuff to kennel

---
+++ kennel/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Attendre que la base de données soit prête
+until pg_isready -h "${PG_HOST}" -p 5432 -U "${PG_USER}"; do
+    echo "En attente de la base de données..."
+    sleep 2
+done
+
+npx prisma migrate deploy
+
+npm run start:prod
