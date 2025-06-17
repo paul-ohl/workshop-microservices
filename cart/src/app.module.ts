fa83fbc734413460d5972f5864ed1b208f69fb44
Paul Ohl <paul.lv.ohl@gmail.com>
2025-06-17 11:27:43 +0200

    feat: Prometheus + Grafana setup

--- cart/src/app.module.ts
+++ cart/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CartModule } from './cart/cart.module';
 import { ItemModule } from './item/item.module';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [CartModule, ItemModule],
+  imports: [CartModule, ItemModule, PrometheusModule.register()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
